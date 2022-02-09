60144545822ef4523f63141af7f2322a39e07cb2
Ahmed Sabaa <ahmed.sabaa@spryker.com>
2022-02-09 11:38:01 +0100

    squashed single decouple commit

--- src/Spryker/Client/ProductCategoryStorage/Storage/ProductAbstractCategoryStorageReader.php
+++ src/Spryker/Client/ProductCategoryStorage/Storage/ProductAbstractCategoryStorageReader.php
@@ -97,10 +97,9 @@ class ProductAbstractCategoryStorageReader implements ProductAbstractCategorySto
     protected function findStorageData(int $idProductAbstract, string $localeName, string $storeName): ?array
     {
         if (ProductCategoryStorageConfig::isCollectorCompatibilityMode()) {
-            $clientLocatorClass = Locator::class;
-            /** @var \Generated\Zed\Ide\AutoCompletion&\Spryker\Shared\Kernel\LocatorLocatorInterface $locator */
-            $locator = $clientLocatorClass::getInstance();
-            $productClient = $locator->product()->client();
+            $clientLocatorClassName = Locator::class;
+            /** @var \Spryker\Client\Product\ProductClientInterface $productClient */
+            $productClient = $clientLocatorClassName::getInstance()->product()->client();
             // TODO: $storeName should be used here too.
             $collectorData = $productClient->getProductAbstractFromStorageById($idProductAbstract, $localeName);
             $categories = [];
