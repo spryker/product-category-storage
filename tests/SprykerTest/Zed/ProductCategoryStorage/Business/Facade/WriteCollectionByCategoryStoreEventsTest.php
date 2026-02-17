339920989f9e483142d8481786f9c5df2851e231
Serhii Chepela <serhii.chepela@spryker.com>
2026-02-17 19:18:09 +0200

    FRW-11296 Symfony Scheduler (#265)
    FRW-11296 Integrate Symfony Scheduler into Spryker and replace the existing SchedulerJenkins module.

--- tests/SprykerTest/Zed/ProductCategoryStorage/Business/Facade/WriteCollectionByCategoryStoreEventsTest.php
+++ tests/SprykerTest/Zed/ProductCategoryStorage/Business/Facade/WriteCollectionByCategoryStoreEventsTest.php
@@ -75,6 +75,7 @@ class WriteCollectionByCategoryStoreEventsTest extends Unit
         $this->tester->setDependency(QueueDependencyProvider::QUEUE_ADAPTERS, function (Container $container) {
             return [
                 $this->tester->getLocator()->rabbitMq()->client()->createQueueAdapter(),
+                $container->getLocator()->symfonyMessenger()->client()->createQueueAdapter(),
             ];
         });
     }
